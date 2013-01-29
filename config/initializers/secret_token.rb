6f0c308825eb149c801c882a9941b955301313c1
vikas <vikasyaligar.it@gmail.com>
2013-01-29 15:26:35 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CoursePlan::Application.config.secret_token = '5f76257c64fec3e1c40538bb288ecc4cf4c007fc83b5d3bbf19b11c779e376e872086789b07680ce87809945d0522e9d5315ee37dee3afef608a48474a6c9264'
